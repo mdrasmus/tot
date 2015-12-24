a0fea890ea7ad07a11a1fe403a7f4dbc29ecf0ad
Matt Rasmussen <matt.rasmus@gmail.com>
2015-12-24 07:31:40 -0800

    config fuse in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@
 sudo apt-get update
 
 sudo apt-get install -y python-virtualenv
+
+sudo su <<EOF
+grep '^user_allow_other' /etc/fuse.conf > /dev/null || (
+    echo 'user_allow_other' >> /etc/fuse.conf
+)
+EOF
